3d213a997422762adcab0a35ef628d80246e9061
TenzinCS <90011675+TenzinDhekyung@users.noreply.github.com>
2021-10-18 19:56:52 -0400

    Update and rename README.md to .py

---
+++ .py
@@ -0,0 +1,30 @@
+# my-first-repository
+Python Calculator
+# Building a basic calculator in Python
+
+print("Select an operation: ")
+print("1. ADD")
+print("2. SUBTRACT")
+print("3. MULTIPLY")
+print("4. DIVIDE")
+
+operation=input()
+
+if operation=="1":
+    num1=input("Enter first number: ")
+    num2=input("Enter second number: ")
+    print("The sum is "+str(float(num1)+float(num2)))
+elif operation=="2":
+    num1=input("Enter first number: ")
+    num2=input("Enter second number: ")
+    print("The difference is "+str(float(num1)-float(num2)))
+elif operation=="3":
+    num1=input("Enter first number: ")
+    num2=input("Enter second number: ")
+    print("The product is "+str(float(num1)*float(num2)))
+elif operation== "4":
+    num1=input("Enter first number: ")
+    num2=input("Enter second number: ")
+    print("The result is"+str(float(num1)/float(num2)))
+else:
+    print("Invalid Entry")
--- README.md
+++
@@ -1,2 +0,0 @@
-# my-first-repository
-Python Calculator
